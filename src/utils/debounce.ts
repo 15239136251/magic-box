cb5c7532fd65a1c987abdba1645ed52bc52f4f74
qu.hb@sungeon.com <qu.hb@sungeon.com>
2023-02-09 15:05:48 +0800

    ✨ feat(添加节流方法):

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,8 +1,18 @@
 // 防抖
 let timeout = 0
-export let debounce = (fnc: Function, params?: any) => {
+export const debounce = (fnc: Function, params?: any) => {
     clearTimeout(timeout)
     timeout = setTimeout(() => {
         fnc(params)
     }, 500)
+}
+
+// 节流
+let throttleTime = 0
+export const throttle = (fnc: Function, time: number = 300) => {
+    if (throttleTime) return
+    throttleTime = setTimeout(() => {
+        fnc()
+        throttleTime = 0
+    }, time)
 }
