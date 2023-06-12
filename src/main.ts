325d9cba298ded63813816a79f644d7bfeb5a1bd
15239136251 <2532281393@qq.com>
2023-06-12 17:22:15 +0800

    引入icon组件

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as Pinia from 'pinia'
 import piniaPersist from '@/plugins/pinia-plugin-persist'
 
 // #ifdef H5 || APP-PLUS
-import { Popup, Cell, CellGroup, Field, Button } from 'vant'
+import { Popup, Cell, CellGroup, Field, Button, Icon } from 'vant'
 import 'vant/lib/index.css'
 // #endif
 import 'virtual:windi.css'
@@ -25,6 +25,7 @@ export function createApp() {
   app.use(CellGroup)
   app.use(Field)
   app.use(Button)
+  app.use(Icon)
   // #endif
   return {
     app,
